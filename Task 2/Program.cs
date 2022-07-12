e148b08a19294c16a49830aa11880fddd7e8c13b
belevgen <belsky_evgeniy@mail.rum>
2022-07-12 21:56:27 +0600

    ДЗ Семинар 9_04.07.22_Задача 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string a_String = Console.ReadLine();
+    return int.Parse(a_String);
+}
+
+int PrintSum1Numbers(int numberM, int numberN)
+{
+    if (numberM > numberN) return 0;
+
+    return PrintSum1Numbers(numberM, numberN-1) + numberN;
+}
+
+int numberM = Prompt("Введите первое число: ");
+int numberN = Prompt("Введите второе число: ");
+
+System.Console.WriteLine();
+
+int a = PrintSum1Numbers(numberM, numberN);
+System.Console.WriteLine($"Произведение натуральных чисел от {numberM} до {numberN} равняется {a}" );
+
